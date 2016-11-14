742b92450a94a8eb7b931767f5d2c2bfb2c0e783
Trevor <t.perrier@gmail.com>
2016-11-13 23:03:22 -0800

    fix(php) fix php post and add basic usss.php

--- static/post.php
+++ static/post.php
@@ -1,27 +1,31 @@
 <?php
-header('Content-Type: application/json');
 
 $request_body = file_get_contents('php://input');
 $data = json_decode($request_body,TRUE);
 $url = $data['url'];
 
-$payload = json_encode([
+$payload = http_build_query([
   'sessionId' => $data['sessionId'],
   'serviceCode' => $data['serviceCode'],
   'phoneNumber' => $data['phoneNumber'],
-  'text' => $data['text']
+  'text' => $data['text'] 
 ]);
+
+// echo var_dump($data);
+// echo $payload;
 
 $ch = curl_init($url);
 curl_setopt($ch, CURLOPT_POST, TRUE);
 curl_setopt($ch, CURLOPT_POSTFIELDS, $payload);
 curl_setopt($ch, CURLOPT_RETURNTRANSFER, TRUE);
-curl_setopt($ch, CURLOPT_HTTPHEADER, 'Content-Type: application/json');
 
 $response = curl_exec($ch);
 curl_close($ch);
 
-echo $response;
-echo json_encode( [ 'action' => 'con' , 'text' => $payload['sessionId'] ]);
+$action = substr($response,0,3);
+$text = substr($response,4);
+
+header('Content-Type: application/json');
+echo json_encode( [ 'action' => $action , 'text' => $text ]);
 
 ?>
